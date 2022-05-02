2dd43c62e817126c1d963a42874ebac4b6dff065
Nikita Sharma <nikita@cs.utexas.edu>
2022-05-02 16:18:38 -0500

    add initial TLB SRRIP impl

---
+++ clients/drcachesim/simulator/smart_tlb.cpp
@@ -0,0 +1,78 @@
+/* **********************************************************
+ * Copyright (c) 2015-2021 Google, Inc.  All rights reserved.
+ * **********************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Google, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+#include "smart_tlb.h"
+#include "../common/utils.h"
+#include <assert.h>
+
+void
+smart_tlb_t::access_update(int block_idx, int way)
+{
+    // set RRPV to 0
+    get_caching_device_block(block_idx, way).counter_ = NEAR_IMM_RRPV;
+}
+
+int
+caching_device_t::replace_which_way(int block_idx)
+{
+    // The base caching device class only implements LFU.
+    // A subclass can override this and access_update() to implement
+    // some other scheme.
+    int max_way = 0;
+    int max_rrpv = 0;
+    for (int way = 0; way < associativity_; ++way) {
+        auto block = get_caching_device_block(block_idx, way);
+
+        // extra space is available, use it
+        if (block.tag_ == TAG_INVALID) {
+            // set a _distant_ rrpv value for incoming way
+            get_caching_device_block(block_idx, min_way).counter_ = LONG_RRPV;
+            return max_way;
+        }
+        
+        // find left-most cache block with max RRPV value
+        else if (block.counter_ > max_rrpv) {
+            max_rrpv = block.counter_;
+            max_way = way;
+        }
+    }
+
+    // update RRPV values
+    for (int way = 0; way < associativity_; ++way) {
+        auto block = get_caching_device_block(block_idx, way);
+        block.counter_ += (DISTANT_RRPV - max_rrpv);
+    }
+
+    // set a _distant_ rrpv value for incoming way
+    get_caching_device_block(block_idx, min_way).counter_ = LONG_RRPV;
+    return max_way;
+}
